46a7bb77594ae9feee38594b2f22aa9de1b3de49
Alexis Rivas <sixela.savir@gmail.com>
2018-10-17 14:35:55 -0300

    doc currency class

--- app/src/main/java/com/mercadolibre/www/mercadopago/mvp/core/Currency.java
+++ app/src/main/java/com/mercadolibre/www/mercadopago/mvp/core/Currency.java
@@ -4,6 +4,11 @@ public class Currency {
 
     private static final String TAG = Currency.class.getName();
 
+    /**
+     * Method for convert string variable to float variable
+     * @param n string value at entry
+     * @return float type variable that represents the converted number
+     */
     public static float convertStringToFloat(String n) {
         float num = 0;
         try {
